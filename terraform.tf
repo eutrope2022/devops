9ecfe1969cafdd2c0a18b5fa9b9db7e19a64ae52
eutrope2022 <110071369+eutrope2022@users.noreply.github.com>
2022-07-26 17:38:35 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,15 @@
 
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAX53EWVBGJAN4LQG7"
+  secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+  ami  = "ami-0ab4d1e9cf9a1215a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
